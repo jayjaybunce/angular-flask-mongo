2f7c83a29ac2c600e46d94321eefad52441fa59a
Jerrod Bunce <jbunce@trucoin.com>
2015-01-07 00:10:23 -0700

    no message

--- angular_flask/core.py
+++ angular_flask/core.py
@@ -5,7 +5,7 @@ from angular_flask import app
 from flask.ext.restless import APIManager
 from flask.ext.mongoengine import MongoEngine
 
-app.config["MONGODB_SETTINGS"] = {"host":os.environ.get('MONGOLAB_URI')}
+app.config["MONGODB_SETTINGS"] = {"host":"mongodb://heroku_app33066573:dtnnejfhktva4o84jcrbv3cmqv@ds031721.mongolab.com:31721/heroku_app33066573"}
 
 
 mongo_db = MongoEngine(app)
