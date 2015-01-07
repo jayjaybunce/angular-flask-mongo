5dff715d158b63b642f8bf6face3390940441cc6
Jerrod Bunce <jbunce@trucoin.com>
2015-01-06 23:50:16 -0700

    no message

--- angular_flask/core.py
+++ angular_flask/core.py
@@ -5,12 +5,8 @@ from angular_flask import app
 from flask.ext.restless import APIManager
 from flask.ext.mongoengine import MongoEngine
 
-db_url = os.environ.get('DATABASE_URL')
-print db_url
-app.config["MONGODB_SETTINGS"] = {'db': "intersect",'host':db_url}
-app.config["SECRET_KEY"] = "KeepThisS3cr3t"
+app.config['MONGOLAB_URI'] = os.environ.get('MONGOLAB_URI')
 
 mongo_db = MongoEngine(app)
 
 api_manager = APIManager(app)
-
